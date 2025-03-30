d25c172fc0cdc8f251d2fd6dd0ebff90a632b5d8
Lakulength@1717 <girm2002@gmail.com>
2025-03-30 19:53:43 -0400

    .gitingore added

---
+++ server/dist/config/connection.js
@@ -0,0 +1,30 @@
+import dotenv from 'dotenv';
+dotenv.config();
+import { Sequelize } from 'sequelize';
+// Ensure proper typing for environment variables
+const isProduction = process.env.NODE_ENV === 'production';
+// Type definition for Sequelize
+const sequelize = process.env.DB_URL
+    ? new Sequelize(process.env.DB_URL, {
+        dialect: 'postgres',
+        dialectOptions: isProduction
+            ? {
+                ssl: {
+                    require: true, // SSL is required in production (Render)
+                    rejectUnauthorized: false, // Disable SSL certificate validation (specific to Render)
+                },
+            }
+            : {},
+    })
+    : new Sequelize(process.env.DB_NAME || '', // Database name
+    process.env.DB_USER || '', // Database user
+    process.env.DB_PASSWORD || '', // Database password (add default empty string for safety)
+    {
+        host: process.env.DB_HOST || 'localhost', // Default to localhost for local development
+        dialect: 'postgres',
+        dialectOptions: {
+            decimalNumbers: true, // Ensure decimal numbers are returned correctly
+        },
+        port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 5432, // Ensure the port is treated as a number
+    });
+export default sequelize;
