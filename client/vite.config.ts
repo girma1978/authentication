d604faadc8d277ee282e1317c880007078fef68c
Lakulength@1717 <girm2002@gmail.com>
2025-03-30 20:17:23 -0400

    client side code modified to adapt to render

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -29,7 +29,6 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   server: {
     port: 3000,
-    open: true,
     proxy: {
       '/api': {
         target: process.env.NODE_ENV === 'production' 
