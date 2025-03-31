3a4b3534921fe35ae6d0448094a046b3cf6fdf1c
Lakulength@1717 <girm2002@gmail.com>
2025-03-30 22:29:49 -0400

    server and client in  the same folder

---
+++ fullstack/server/client/vite.config.ts
@@ -0,0 +1,50 @@
+// import { defineConfig } from 'vite';
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   server: {
+//     port: 3000,
+//     open: true,
+//     proxy: {
+//       '/api': {
+//         target: 'http://localhost:3001',
+//         changeOrigin: true,
+//         secure: false,
+//       },
+//       '/auth': {
+//         target: 'http://localhost:3001',
+//         changeOrigin: true,
+//         secure: false
+//       },
+//     },
+//   },
+// });
+
+
+
+
+import { defineConfig } from 'vite';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  server: {
+    port: 3000,
+    proxy: {
+      '/api': {
+        target: process.env.NODE_ENV === 'production' 
+          ? 'https://authentication-1-oqwb.onrender.com' 
+          : 'http://localhost:3001',
+        changeOrigin: true,
+        secure: false,
+      },
+      '/auth': {
+        target: process.env.NODE_ENV === 'production' 
+          ? 'https://authentication-1-oqwb.onrender.com' 
+          : 'http://localhost:3001',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+});
+
